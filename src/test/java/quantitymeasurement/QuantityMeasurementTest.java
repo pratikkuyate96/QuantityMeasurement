4ed2f6a5983a3b6f1073fc4bab57c46095daa9fc
pratikkuyate96 <62277306+pratikkuyate96@users.noreply.github.com>
2020-05-22 10:00:39 +0530

    ADDED: Test case for null check

--- src/test/java/quantitymeasurement/QuantityMeasurementTest.java
+++ src/test/java/quantitymeasurement/QuantityMeasurementTest.java
@@ -13,4 +13,11 @@ public class QuantityMeasurementTest {
         boolean isEqual = this.quantityMeasurement.compare(value1, value2);
         Assert.assertEquals(true, isEqual);
     }
+
+    @Test
+    public void givenNullValue_WhenNotEqual_ReturnFalse() {
+        Unit value1 = new Unit(0);
+        boolean isEqual = this.quantityMeasurement.compare(value1, null);
+        Assert.assertEquals(false, isEqual);
+    }
 }
