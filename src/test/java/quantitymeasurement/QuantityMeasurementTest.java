bd659c12d2ddeca037cd5e073ffacb2b5b746551
pratikkuyate96 <62277306+pratikkuyate96@users.noreply.github.com>
2020-05-22 10:09:42 +0530

    ADDED: To check reference is same or not

--- src/test/java/quantitymeasurement/QuantityMeasurementTest.java
+++ src/test/java/quantitymeasurement/QuantityMeasurementTest.java
@@ -20,4 +20,11 @@ public class QuantityMeasurementTest {
         boolean isEqual = this.quantityMeasurement.compare(value1, null);
         Assert.assertEquals(false, isEqual);
     }
+
+    @Test
+    public void givenSameReferance_WhenEqual_ReturnTrue() {
+        Unit value1 = new Unit(0);
+        boolean isEqual = this.quantityMeasurement.compare(value1, value1);
+        Assert.assertEquals(true, isEqual);
+    }
 }
