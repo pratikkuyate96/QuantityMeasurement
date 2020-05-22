232de8e9e4b8db3d5acd45a0e34fe7f8771725fb
pratikkuyate96 <62277306+pratikkuyate96@users.noreply.github.com>
2020-05-22 09:36:07 +0530

    ADDED: To compare two value

--- src/main/java/quantitymeasurement/QuantityMeasurement.java
+++ src/main/java/quantitymeasurement/QuantityMeasurement.java
@@ -2,4 +2,11 @@ package quantitymeasurement;
 
 public class QuantityMeasurement {
 
+    public double unitValue;
+
+    public boolean compare(Unit value1, Unit value2) {
+        if(value1.equals(value2))
+            return true;
+        return false;
+    }
 }
